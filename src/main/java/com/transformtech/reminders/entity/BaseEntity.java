24648b0f5ec444ff06a07a444e0c48a136c92c13
baongoc29062003 <hbna3bc@gmail.com>
2026-01-05 17:28:10 +0700

    Update BaseEntity

--- src/main/java/com/transformtech/reminders/entity/BaseEntity.java
+++ src/main/java/com/transformtech/reminders/entity/BaseEntity.java
@@ -30,6 +30,9 @@ public abstract class BaseEntity {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public LocalDateTime getCreatedDate() {
         return createdDate;
